c5efae8d38b84c1adba3d71eb0d238949e3751ec
CAPYBARA <dltjdrnjs3@naver.com>
2025-01-29 22:18:09 +0900

    Create ShohagLovesXOR.kt

---
+++ kotlin/src/2039/C1/ShohagLovesXOR.kt
@@ -0,0 +1,18 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+    val t = reader.readLine().toInt()
+    for(i in 0 until t) {
+        val (x, m) = reader.readLine().split(" ").map { it.toLong()}
+        Long
+        println("$x $m")
+    }
+    writer.flush()
+    writer.close()
+
+}
