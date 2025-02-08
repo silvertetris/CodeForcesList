c70e9eec4b7d44c6cac38105e122b45654c6ffd1
CAPYBARA <dltjdrnjs3@naver.com>
2025-02-08 21:15:24 +0900

    Update MeaningMean.kt

--- kotlin/src/2021/A/MeaningMean.kt
+++ kotlin/src/2021/A/MeaningMean.kt
@@ -1,21 +1,18 @@
-import java.io.BufferedWriter
-import java.io.OutputStreamWriter
-
 fun main() {
     val t = readln().toInt()
-    val writer = BufferedWriter(OutputStreamWriter(System.out))
+    val writer = System.out.bufferedWriter()
     for (i in 0 until t) {
         var n = readln().toInt()
         var arr = readln().split(" ").map { it.toLong() }.toMutableList()
-        while(true) {
+        while (true) {
             arr.sort()
-            if(arr.size==2) {
-                writer.write("${(arr[0]+arr[1])/2}\n")
+            if (arr.size == 2) {
+                writer.write("${(arr[0] + arr[1]) / 2}\n")
                 break
             }
-            val temp =(arr[0]+arr[arr.lastIndex-1])/2
+            val temp = (arr[0]+arr[1])/2
+            arr.removeAt(1)
             arr.removeAt(0)
-            arr.removeAt(arr.lastIndex-1)
             arr.add(temp)
         }
     }
