917d7c5643f47b6652407cb29e0ca4e872816e00
CAPYBARA <dltjdrnjs3@naver.com>
2025-03-04 18:57:57 +0900

    Update Limited Repainting.kt

--- kotlin/src/2070/C/Limited Repainting.kt
+++ kotlin/src/2070/C/Limited Repainting.kt
@@ -1,5 +1,3 @@
-import kotlin.math.max
-
 fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
@@ -9,32 +7,31 @@ fun main() {
         val (n, k) = br.readLine().split(" ").map { it.toInt() }
         val s = br.readLine()//color
         val a = br.readLine().split(" ").map { it.toInt() } // penalty
-        var l = -1
-        var r = Int.MAX_VALUE
-        while(l+1 <r) {
-            val mid = (l+r) /2
-            var nk = k
-            var lst = 0
-            for(i in 0 until n) {
-                if(a[i]>mid && s[i]=='R') {
-                    lst = 0
-                }
-                else if(a[i]>mid && s[i]=='B') {
-                    if(lst==0) {
-                        nk-=1
-                        lst=1
-                    } else {
-                        lst = 1
+        var l = 0
+        var r = a.max()
+        while (l <= r) { //우리가 탐색하는 기준은 penalty max(min)
+            val mid = (l + r) / 2
+            var nk = k //남은 k 값
+            var lst = false//그냥 포함시켜도 될지에 대한 불린
+            for (i in 0 until n) {
+                if (a[i] > mid && s[i] == 'R') {//lst 가 false 인순간 칠해야함
+                    lst = false
+                } else if (a[i] > mid && s[i] == 'B') {
+                    if (!lst) {
+                        nk -= 1
+                        lst = true
+                    } else { //작으면 무시해도 되니까 횟수를 줄이지 않고 nk-=1
+                        lst = true
                     }
                 }
             }
-            if(nk>=0) {
-                r=mid
-            }else {
-                l=mid
+            if (nk >= 0) {
+                r = mid - 1
+            } else {
+                l = mid + 1
             }
         }
-        bw.write("$r\n")
+        bw.write("$l\n")
     }
     bw.flush()
     bw.close()
