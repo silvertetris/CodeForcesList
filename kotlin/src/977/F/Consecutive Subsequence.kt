3cf351498567a8ac9fd4393c92de6ffd8f45cb00
CAPYBARA <dltjdrnjs3@naver.com>
2025-03-10 22:54:42 +0900

    Update Consecutive Subsequence.kt

--- kotlin/src/977/F/Consecutive Subsequence.kt
+++ kotlin/src/977/F/Consecutive Subsequence.kt
@@ -2,28 +2,28 @@ fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
     val n = br.readLine().toInt()
-    val arr= br.readLine().split(" ").map { it.toInt() }
-    var answer = 0L
+    val arr = br.readLine().split(" ").map { it.toInt() }
+    var answer = 0
     var current = 0
-    var vec = ArrayList<Int>()
-    val dp = LongArray(200001){0}
-    for(i in 0 until n ) {
-        dp[arr[i]] = maxOf(dp[arr[i]], dp[arr[i]-1]+1)
-        if(dp[arr[i]]>answer) {
-            answer = dp[arr[i]]
+    val vec = ArrayList<Int>()
+    val dp: MutableMap<Int, Int> = sortedMapOf()
+    for (i in 0 until n) {
+        dp[arr[i]] = maxOf(dp[arr[i]]?:0, (dp[arr[i] - 1]?:0) + 1)
+        if (dp[arr[i]]!! > answer) {
+            answer = dp[arr[i]]!!
             current = arr[i]
         }
     }
-    for(i in n-1 downTo 0) {
-        if(arr[i] == current) {
+    for (i in n - 1 downTo 0) {
+        if (arr[i] == current) {
             current--
             vec.add(i)
         }
     }
-    bw.write("$current\n")
+    bw.write("${vec.size}\n")
     vec.sort()
-    for(i in 0 until vec.size) {
-        bw.write("${vec[i]+1} ")
+    for (i in 0 until vec.size) {
+        bw.write("${vec[i] + 1} ")
     }
     bw.flush()
     bw.close()
