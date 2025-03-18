da00d312cde814891ea47354e230bb605075b9f7
CAPYBARA <dltjdrnjs3@naver.com>
2025-03-18 21:20:57 +0900

    Update Two Colors.kt

--- kotlin/src/2075/C/Two Colors.kt
+++ kotlin/src/2075/C/Two Colors.kt
@@ -2,53 +2,38 @@ fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
     val t = br.readLine().toInt()
-    fun binarySearch(a: List<Int>, target: Int): Int {
-        var low = 0
-        var high = a.size
-        while (low < high) {
-            val mid = (low + high) / 2
-            if (a[mid] >= target) {
-                high = mid
-            } else {
-                low = mid + 1
-            }
-        }
-        return low
-    }
     repeat(t) {
         val (n, m) = br.readLine().split(" ").map { it.toInt() }
-        val a = br.readLine().split(" ").map { it.toInt() }.sorted()
-        var totalWays: Long = 0
+        val a = br.readLine().split(" ").map { it.toInt() }.toIntArray()
 
-        val sumArray = LongArray(m + 1)
-        for (i in 0 until m) {
-            sumArray[i + 1] = sumArray[i] + a[i]
+        for (i in a.indices) { //경우의 수의 최대 가질 수 있는 수는 n-1임
+            if (a[i] == n) a[i]--//n이면 경우의 수 초과가남 -> 하나 줄여줌 (최소 두가지 색으로 칠해야 하니까)
         }
 
+        a.sort()
+
+        val s = LongArray(m + 1)
+        for (i in m - 1 downTo 0) { //suffixSum
+            s[i] = s[i + 1] + a[i]
+        }
+
+        var ans = 0L
         for (i in 0 until m) {
-            val temp = minOf(n - 1, a[i])
+            var l = i + 1
+            var r = m - 1
 
-            val l1 = binarySearch(a, n - 1)
-            val cnt1 = (m - l1) - if (i in l1..<m) 1 else 0
-            totalWays += cnt1 * temp
-
-            val lowerBound = n - temp - 1
-            val upperBound = n - 1
-            val l2 = binarySearch(a, lowerBound)
-            val r2 = binarySearch(a, upperBound)
-
-            var cnt2 = r2 - l2
-            var sum = sumArray[r2] - sumArray[l2]
-
-            if (i in l2..<r2) {
-                cnt2 -= 1
-                sum -= a[i]
+            while (l <= r) {
+                val mid = (l + r) / 2
+                if (a[mid] + a[i] >= n) {
+                    r = mid - 1
+                } else {
+                    l = mid + 1
+                }
             }
-
-            totalWays += cnt2 * (temp - n + 1).toLong() + sum
+            ans += s[l] + (m - l) * (a[i] - n + 1).toLong() //경우의 수 점화식 -> 이는 조합론에서 직접 생각해야함
         }
 
-        bw.write("$totalWays\n")
+        bw.write("${2 * ans}\n")
     }
     bw.flush()
     bw.close()
