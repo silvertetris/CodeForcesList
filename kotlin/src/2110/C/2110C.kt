ca13c9a8020bcd9121eecec5f02d19e609c9cb31
silvertetris <dltjdrnjs3@naver.com>
2025-05-25 23:34:35 +0900

    Update 2110C.kt

--- kotlin/src/2110/C/2110C.kt
+++ kotlin/src/2110/C/2110C.kt
@@ -1,102 +1,48 @@
 fun main() {
     val br = System.`in`.bufferedReader()
-    val bw = System.out.bufferedWriter()
+    val bw = System.`out`.bufferedWriter()
     val t = br.readLine().toInt()
-
     repeat(t) {
         val n = br.readLine().toInt()
-        val d = br.readLine().split(" ").map { it.toInt() }
-
-        val l = IntArray(n)
-        val r = IntArray(n)
-        for (i in 0 until n) {
-            val (a, b) = br.readLine().split(" ").map { it.toInt() }
-            l[i] = a
-            r[i] = b
+        val d = br.readLine().split(" ").map { it.toInt() }.toIntArray()
+        val li = IntArray(n) {0}
+        val ri = IntArray(n) {0}
+        for(i in 0 until n) {
+            val temp = br.readLine().split(" ").map { it.toInt() }.toIntArray()
+            li[i] = temp[0]
+            ri[i] = temp[1]
         }
-
-        var tempLow = 0
-        var tempHigh = 0
-
-        val leastH = IntArray(n + 1) { -1 }
-        val maxH = IntArray(n + 1) { -1 }
-        leastH[0] = 0
-        maxH[0] = 0
-
-        var bool = true
-
-        for (i in 0 until n) {
-            val curD = d[i]
-
-            var nextLow: Int
-            var nextHigh: Int
-
-            when (curD) {
-                0 -> {
-                    nextLow = tempLow
-                    nextHigh = tempHigh
-                }
-                1 -> {
-                    nextLow = tempLow + 1
-                    nextHigh = tempHigh + 1
-                }
-                else -> {
-                    // -1이면 0 or 1 가능하니까 low는 그대로, high는 1 더한값
-                    nextLow = tempLow
-                    nextHigh = tempHigh + 1
-                }
-            }
-
-            val nextLeastH = maxOf(nextLow, l[i])
-            val nextMaxH = minOf(nextHigh, r[i])
-
-            if (nextLeastH > nextMaxH) {
-                bool = false
-                break
-            }
-            //싹다 대입
-            leastH[i + 1] = nextLeastH
-            maxH[i + 1] = nextMaxH
-
-            tempLow = nextLeastH
-            tempHigh = nextMaxH
-        }
-
-        //#4. final answer
-        if (!bool) {
-            bw.write("-1\n")
-            return@repeat
-        }
-
-        val answer = IntArray(n)
-        var curH = leastH[n]
-
-        for (i in n - 1 downTo 0) {
-            val prevLeastH = leastH[i]
-            val prevMaxH = maxH[i]
-
-            if (curH in prevLeastH..prevMaxH) {
-                answer[i] = 0
-            } else if (curH - 1 in prevLeastH..prevMaxH) {
-                answer[i] = 1
-                curH--
+        var left = 0
+        val last = mutableListOf<Int>()
+        for(i in 0 until n) {
+            if(d[i]==-1) {
+                last.add(i) //인덱스를 더함
             } else {
-                bw.write("-1\n")
-                return@repeat
+                left+=d[i] //좌 기준으로 일때 높이 더함
+            }
+            while(left<li[i]) { //각 상황 가정
+                if(last.isEmpty()) { //추가할게 없는데 조건이 만족 못하면 안되는 배열임
+                    bw.write("-1\n")
+                    return@repeat
+                }
+                d[last.last()]=1//백트레킹
+                left++
+                last.removeLast()
+            }
+            while(left+last.size>ri[i]) {
+                if(last.isEmpty()) {
+                    bw.write("-1\n")
+                    return@repeat
+                }
+                d[last.last()]=0
+                last.removeLast()
+            }
+            if(d[i]==-1) {
+                d[i] = 0
             }
         }
-
-        // 원래 d_i 랑 맞는지 아닌지 확인
-        for (i in 0 until n) {
-            if (d[i] != -1 && d[i] != answer[i]) {
-                bw.write("-1\n")
-                return@repeat
-            }
-        }
-
-        bw.write("${answer.joinToString(" ")}\n")
+        bw.write("${d.joinToString(" ")}\n")
     }
     bw.flush()
     bw.close()
-    br.close()
 }
