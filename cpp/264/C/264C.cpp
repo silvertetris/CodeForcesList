756ab1f98f4052ba16b40ea556a1520c36d0de49
sean <dltjdrnjs3@naver.com>
2025-06-27 20:16:52 +0900

    Update 264C.cpp

--- cpp/264/C/264C.cpp
+++ cpp/264/C/264C.cpp
@@ -1,38 +1,47 @@
+#include <vector>
 #include <iostream>
 using namespace std;
+using ll = long long;
 
-#define int ll
-typedef long long ll;
-int const N = 1e5 + 20, INF = 1e15;
-int n, q, c[N], v[N], ans[N];
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
 
-int32_t main(){
-    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int n, q;
     cin >> n >> q;
-    for (int i = 0; i < n; i ++) cin >> v[i];
-    for (int i = 0; i < n; i ++) cin >> c[i], c[i] --;
+    vector<ll> v(n);
+    for(int i = 0; i < n; i++){
+        cin >> v[i];
+    }
+    vector<int> c(n);
+    for(int i = 0; i < n; i++){
+        cin >> c[i];
+    }
 
-    while (q --){
-        int a, b; cin >> a >> b;
-        int mx1 = 0, mx2 = 0;
+    const ll INF = LLONG_MIN / 4;
 
-        for (int i = 0; i < n; i ++) ans[i] = -INF;
-        for (int i = 0; i < n; i ++){
-            if (mx1 == ans[c[i]]){
-                ans[c[i]] = max(ans[c[i]], ans[c[i]] + a * v[i]);
-                ans[c[i]] = max(ans[c[i]], mx2 + b * v[i]);
-                mx1 = max(mx1, ans[c[i]]);
+    while(q--){
+        ll a, b;
+        cin >> a >> b;
+        vector<ll> dp(n+1, INF);
+        ll mx1 = 0;
+        ll mx2 = 0;
 
-            }
-            else {
-                ans[c[i]] = max(ans[c[i]], ans[c[i]] + a * v[i]);
-                ans[c[i]] = max(ans[c[i]], mx1 + b * v[i]);
-                mx2 = max(mx2, ans[c[i]]);
-
-                if (mx2 > mx1) swap(mx1, mx2);
+        for(int i = 0; i < n; i++) {
+            if (mx1 == dp[c[i]]) { //해당 최대 값이 그 전 공에서 고른 최대값이니까 성립됨
+                dp[c[i]] = max(dp[c[i]], dp[c[i]] + a * v[i]);
+                dp[c[i]] = max(dp[c[i]], mx2 + b*v[i]); //다른 경우는 mx2를 고려해야함
+                mx1 = max(mx1, dp[c[i]]);
+            } else { //만약 최대값이 그 해당 인덱스 값과 다르다면
+                dp[c[i]] = max(dp[c[i]], dp[c[i]] + a * v[i]);
+                dp[c[i]] = max(dp[c[i]], mx1 + b * v[i]); //이번에는 mx1을 고려해야함
+                mx2 = max(mx2, dp[c[i]]);
+                if (mx2>mx1) swap(mx1, mx2); //더 크면 둘이 swap
             }
         }
-
-        cout << mx1 << '\n';
+        cout<<mx1<<'\n'; //결과적으로 mx1이 최대 값
     }
+
+    return 0;
 }
