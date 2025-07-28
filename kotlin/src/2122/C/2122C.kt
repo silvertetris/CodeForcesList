7dd3cd56a5ef4309ad2894ffc69ef8044e2b129d
sean <dltjdrnjs3@naver.com>
2025-07-28 21:29:02 +0900

    Update 2122C.kt

--- kotlin/src/2122/C/2122C.kt
+++ kotlin/src/2122/C/2122C.kt
@@ -1,15 +1,26 @@
 fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
-    val t= br.readLine().toInt()
+    val t = br.readLine().toInt()
     repeat(t) {
         val n = br.readLine().toInt()
         val x = IntArray(n)
         val y = IntArray(n)
-        for(i in 0 until n) {
-            var temp = br.readLine().split(" ").map { it.toInt()}
-            x[i] = temp[0]
-            y[i] = temp[1]
+        repeat(n) { i ->
+            val (xi, yi) = br.readLine().split(' ').map{it.toInt()}
+            x[i] = xi
+            y[i] = yi
+        }
+        val order = MutableList(n) { it }
+        order.sortBy { x[it] }
+        println(order.joinToString(" "))
+        val half = n / 2
+        order.subList(0, half).sortBy { y[it] }
+        order.subList(half, n).sortBy { y[it] }
+        for (i in 0 until half) {
+            bw.write("${order[i] + 1} ${order[n - 1 - i] + 1}\n")
         }
     }
+    bw.flush()
+    bw.close()
 }
