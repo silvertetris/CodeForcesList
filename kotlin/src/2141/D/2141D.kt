90e08e1f791f78f3d476a59e2d9b50514e499222
sean <dltjdrnjs3@naver.com>
2025-09-17 20:07:23 +0900

    Update 2141D.kt

--- kotlin/src/2141/D/2141D.kt
+++ kotlin/src/2141/D/2141D.kt
@@ -8,11 +8,22 @@ fun main() {
         var k = line[1].toLong()
 
         val a = br.readLine().split(" ").map{it.toLong()}.toLongArray().sortedDescending()
-        /*
-        그니까 이게 기준의 한 수를 잡고
-        그 수까지 만드는데 필요한 비용을 계산해야함
-        dp로 해볼까?
-         */
+        //greedy 기법 ->
+        //#1. 안되는 경우 -> (n * max -sum <=k) -> 이 조건이 만족되면 가능한 경우의 수임
+        if(n*a.max()-a.sum() >k) {
+            bw.write("-1\n")
+            return@repeat
+        }else { //#2. minimize the bad operation
+            val min = a.min()
+            //초과가능한 k 고려
+            val temp = (k+a.sum())/n
+            var ans = a.sumOf { temp -it } - (temp - min)
+            if(min<temp) {
+                ans-=a.count{it==min}-1
+            }
+            bw.write("$ans\n")
+        }
     }
     bw.flush()
+    bw.close()
 }
