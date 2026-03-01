e12198f7cc9ae9bd02be88e853747facd8891b04
silvertetris <dltjdrnjs3@naver.com>
2026-03-01 12:38:55 +0900

    Update 2205D.kt

--- kotlin/src/2205/D/2205D.kt
+++ kotlin/src/2205/D/2205D.kt
@@ -5,7 +5,7 @@ fun main() {
     repeat(t) {
         val n = br.readLine().toInt()
         val a = br.readLine().split(" ").map { it.toInt() }.toIntArray()
-        //dp문제다 이거 무조건
-        
+        val del = IntArray(n) {0}
+
     }
 }
