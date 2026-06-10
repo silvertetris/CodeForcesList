8d00b9213dc590ebde9ecce5bd3f67a15fffed5c
sean <dltjdrnjs3@naver.com>
2026-06-10 20:34:38 +0900

    Update 2233C.cpp

--- cpp/2233/C/2233C.cpp
+++ cpp/2233/C/2233C.cpp
@@ -21,168 +21,35 @@ int main()
         cin >> n >> k;
         string s = "";
         cin >> s;
-        int firstidx = 0;
-        int secondidx = n - 1;
-        int entirefirst = 0;
-        int entiresecond = 0;
-        vector<bool> res(n, false);
-        for (int i = 0; i < n; i++)
-        {
-            if (s[i] == '(')
-            {
-                firstidx = i;
-                break;
+        int minval =0;
+        int pos=0;
+        int curval = 0;
+        for(int i=0; i<n; i++) {
+            if(s[i]=='(') {
+                curval++;
+            }else {
+                curval--;
+            }
+            if(curval<minval) {
+                pos = i;
+                minval = curval;
             }
         }
-        for (int i = n - 1; i >= 0; i--)
-        {
-            if (s[i] == ')')
-            {
-                secondidx = i;
-                break;
+        vector<int> res(n, 0);
+        for(int i=0; i<pos; i++) {
+            if(k>0 &&s[i] =='(') {
+                k--;
+                res[i] = 1;
             }
         }
-        for (int i = firstidx; i <= secondidx; i++)
-        {
-            if (s[i] == ')')
-            {
-                entiresecond++;
-            }
-            else
-            {
-                entirefirst++;
-            }
-        }
-        int cnt = 0;
-        while (true)
-        {
-            if (firstidx >= secondidx || cnt >= k)
-                break;
-            int a = 0;
-            int b = 0;
-            for (int i = firstidx; i <= secondidx; i++)
-            {
-                if (s[i] == '(')
-                {
-                    a++;
-                }
-                else
-                {
-                    break;
-                }
-            }
-            for (int i = secondidx; i >= firstidx; i--)
-            {
-                if (s[i] == ')')
-                {
-                    b++;
-                }
-                else
-                {
-                    break;
-                }
-            }
-            bool flag = false;
-            if (a > b)
-            {
-                for (int i = secondidx; i >= firstidx; i--)
-                {
-                    secondidx--;
-
-                    if (cnt >= k)
-                        break;
-                    if (s[i] == ')' && !flag)
-                    {
-                        res[i] = true;
-                        cnt++;
-                        entiresecond--;
-                    }
-                    else if (s[i] == '(')
-                    {
-                        flag = true;
-                        entirefirst--;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-            }
-            else if (a < b)
-            {
-                for (int i = firstidx; i <= secondidx; i++)
-                {
-                    firstidx++;
-                    if (cnt >= k)
-                        break;
-                    if (s[i] == '(' && !flag)
-                    {
-                        res[i] = true;
-                        cnt++;
-                        entirefirst--;
-                    }
-                    else if (s[i] == ')')
-                    {
-                        flag = true;
-                        entiresecond--;
-                    }
-                }
-            }
-            else
-            {
-                if (entirefirst >= entiresecond)
-                {
-                    for (int i = secondidx; i >= firstidx; i--)
-                    {
-                        secondidx--;
-
-                        if (cnt >= k)
-                            break;
-                        if (s[i] == ')' && !flag)
-                        {
-                            res[i] = true;
-                            cnt++;
-                            entiresecond--;
-                        }
-                        else if (s[i] == '(')
-                        {
-                            flag = true;
-                            entirefirst--;
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                }
-                else
-                {
-                    for (int i = firstidx; i <= secondidx; i++)
-                    {
-                        firstidx++;
-                        if (cnt >= k)
-                            break;
-                        if (s[i] == '(' && !flag)
-                        {
-                            res[i] = true;
-                            cnt++;
-                            entirefirst--;
-                        }
-                        else if (s[i] == ')')
-                        {
-                            flag = true;
-                            entiresecond--;
-                        }
-                    }
-                }
+        for(int i=n-1; i>=pos; i--) {
+            if(k>0 && s[i] ==')') {
+                k--;
+                res[i] =1;
             }
         }
         for(int i=0; i<n; i++) {
-            if(res[i]) {
-                cout<<1;
-            }else{
-                cout<<0;
-            }
+            cout<<res[i];
         }
         cout<<"\n";
     }
