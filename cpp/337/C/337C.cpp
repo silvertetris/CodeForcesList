1e0791d44e0a0e7ad0df4aa5def9ca6c90367435
sean <dltjdrnjs3@naver.com>
2026-08-13 19:15:47 +0900

    Create 337C.cpp

---
+++ cpp/337/C/337C.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define ld long double
+
+using namespace std;
+
+int INF = 1e9+9;
+void solve() {
+    int n, m, k;//문제수, 맞춘거, 이만큼 연속으로 맞추면 2배
+    cin>>n>>m>>k;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+
+    cin.tie(0);
+    cout.tie(0);
+
+    int t=1;
+
+    while(t--) {
+        solve();
+    }
+}
