02af61c20f001e599e53eb4dafb9665f8bf0ca0c
sean <dltjdrnjs3@naver.com>
2026-08-13 19:15:43 +0900

    Update 1105C.cpp

--- cpp/1105/C/1105C.cpp
+++ cpp/1105/C/1105C.cpp
@@ -10,19 +10,17 @@ int main() {
     long long n, l, r;
     cin >> n >> l >> r;
 
-    // cnt[j] = [l, r] 안에서 x % 3 == j 인 x의 개수
     long long cnt[3] = {0, 0, 0};
-    for (int j = 0; j < 3; j++) {
-        // l 이상이면서 나머지가 j인 첫 번째 수 찾기
-        long long first = l + ((j - l % 3) + 3) % 3;
-        if (first <= r) {
-            cnt[j] = (r - first) / 3 + 1;
-        }
+    int temp = r - l;
+    cnt[0] = temp/3;
+    cnt[1] = temp/3;
+    cnt[2] = temp/3;
+    for(int i=l+(temp/3)*3; i<=r; i++) {
+        cnt[i%3]++;
     }
-
     // dp[i][j] = 앞에서 i개를 채웠을 때 합이 mod 3 == j 인 경우의 수
     vector<vector<long long>> dp(n + 1, vector<long long>(3, 0));
-    dp[0][0] = 1;  // 0개 선택했을 때 합은 0
+    dp[0][0] = 1;
 
     for (int i = 1; i <= n; i++) {
         for (int j = 0; j < 3; j++) {        // 이전 합의 나머지
@@ -33,5 +31,4 @@ int main() {
     }
 
     cout << dp[n][0] << "\n";
-    return 0;
 }
