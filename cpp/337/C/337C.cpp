efca59811f67dfcd8af02d627a99254b87da30bc
sean <dltjdrnjs3@naver.com>
2026-08-27 22:46:32 +0900

    Update 337C.cpp

--- cpp/337/C/337C.cpp
+++ cpp/337/C/337C.cpp
@@ -6,9 +6,27 @@
 using namespace std;
 
 int INF = 1e9+9;
+ll power (ll base, ll exp) {
+    ll result = 1;
+    base%=INF;
+
+    while(exp>0) {
+        if(exp%2!=0) {
+            result = result * base %INF;
+        }
+        base = base*base %INF;
+        exp/=2;
+    }
+    return result;
+}
 void solve() {
     int n, m, k;//문제수, 맞춘거, 이만큼 연속으로 맞추면 2배
     cin>>n>>m>>k;
+    int x;
+    x = max(0, (m-n+n/k));
+    ll ans = ((2*k)%INF *(power(2, x)-1)%INF)%INF +m%INF - x%INF*k%INF;
+    ans%=INF;
+    cout<<ans<<"\n";
 }
 
 int main() {
